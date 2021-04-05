84945512c480b2eb436945ad1d09ee6c149980ca
Mahitha A <mahitha@everest.engineering>
2021-04-05 16:04:39 +0530

    Use integer type for id

--- db/migrate/20210405084036_create_articles.rb
+++ db/migrate/20210405084036_create_articles.rb
@@ -1,6 +1,6 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
-    create_table :articles, id: :uuid do |t|
+    create_table :articles do |t|
       t.string :title
       t.text :body
 
