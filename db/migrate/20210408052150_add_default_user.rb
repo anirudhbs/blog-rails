71e1727284391fe23a22a7552fa019c77c259600
Anirudh <anirudh.bs@everest.engineering>
2021-04-08 18:27:51 +0530

    Use find_or_initialize_by to create default user

--- db/migrate/20210408052150_add_default_user.rb
+++ db/migrate/20210408052150_add_default_user.rb
@@ -1,6 +1,6 @@
 class AddDefaultUser < ActiveRecord::Migration[5.2]
   def change
-    user = User.find_or_create_by!(email: 'default-user@ee')
+    user = User.find_or_initialize_by(email: 'default-user@ee')
     unless user.created_at
       user.password = 'default_pwd'
       user.save!
