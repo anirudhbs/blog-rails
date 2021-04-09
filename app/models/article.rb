f787d2932481be96223a6630d17040cbf72ddf57
Mahitha A <mahitha@everest.engineering>
2021-04-09 16:50:18 +0530

    Show attachment in article

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,6 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 4 }
   validates :body, presence: true, length: { minimum: 4 }
 
-  has_attached_file :attachment
+  has_attached_file :attachment, styles: { thumb: '100x100#', original: '100x100#' }
   do_not_validate_attachment_file_type :attachment
 end
