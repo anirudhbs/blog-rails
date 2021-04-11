a2fa84793acc430a97bb312666971681b1e63148
Mahitha A <mahitha@everest.engineering>
2021-04-11 13:51:23 +0530

    Update job to print count of the each state of the articles

--- app/jobs/articles_state_count_job.rb
+++ app/jobs/articles_state_count_job.rb
@@ -2,6 +2,8 @@ class ArticlesStateCountJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    puts 'Hello Rails'
+    puts "Drafted articles::#{Article.draft.count}"
+    puts "Submitted articles::#{Article.submit.count}"
+    puts "Published articles::#{Article.publish.count}"
   end
 end
