d60ebefbb5cf4a21a55c7e3ac5932cfe386e3a16
Mahitha A <mahitha@everest.engineering>
2021-04-21 16:16:22 +0530

    Remove range for length check

--- app/contracts/ArticleContract.rb
+++ app/contracts/ArticleContract.rb
@@ -1,7 +1,7 @@
 module Contract
   class ArticleContract < Dry::Validation::Contract
     params do
-      required(:title).value(:string, size?: 2..4)
+      required(:title).value(:string, min_size?: 4)
       required(:body).value(:string, min_size?: 4)
     end
   end
