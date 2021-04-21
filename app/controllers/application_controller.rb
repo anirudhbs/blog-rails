a979fd7f264f5bc1a2250226a0db807812840ce6
Mahitha A <mahitha@everest.engineering>
2021-04-21 12:59:55 +0530

    Add option to select manager while creating a user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :role, :password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :role, :password, :manager_id) }
 
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :role, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :role, :password, :current_password, :manager_id)}
   end
 end
